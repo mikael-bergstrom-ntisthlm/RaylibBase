06fadfb2422862b5174f05704618d7f0fba0de94
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2021-04-19 19:49:34 +0200

    Initial commit

---
+++ RaylibBase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Raylib_cs;
+
+namespace RaylibBase
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Raylib.InitWindow(800, 600, "Raylib base");
+
+      Raylib.SetTargetFPS(60);
+      Raylib.SetExitKey(0); // Disable ESC key exit
+
+      while (!Raylib.WindowShouldClose())
+      {
+        // Logic
+
+        // Drawing
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.GRAY);
+        Raylib.EndDrawing();
+      }
+    }
+  }
+}
